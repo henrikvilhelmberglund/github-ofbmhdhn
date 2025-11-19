33630762678f6ef29749a0056dac9999e1fdbe2c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2025-11-19 17:22:21 +0100

    feat: add snippets

--- src/locales/.wuchale/main.main.en.compiled.js
+++ src/locales/.wuchale/main.main.en.compiled.js
@@ -1,15 +1,1 @@
-
-            export let c = ["This is a sub path","Go to sub","Welcome to SvelteKit",["Visit ",[0,"svelte.dev/docs/kit"]," to read the documentation"],"Hello from module","Load messages from page state","Load messages from props/data","Hello from layout!","Hello from nested layout!"]
-            // only during dev, for HMR
-            let latestVersion = -1
-            // @ts-ignore
-            export function update({ version, data }) {
-                if (latestVersion >= version) {
-                    return
-                }
-                for (const [ index, item ] of data['en'] ?? []) {
-                    c[index] = item
-                }
-                latestVersion = version
-            }
-        
+export let c = ["This is a sub path","Go to sub","Welcome to SvelteKit",["Visit ",[0,"svelte.dev/docs/kit"]," to read the documentation"],"Hello from module","Load messages from page state","Load messages from props/data","SvelteKit","Framework","This is a simple snippet example","Hello from layout!","Hello from nested layout!"]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,51 @@
 <script lang="ts">
     import { page } from '$app/state'
     import { greet } from './hello.svelte.js'
+
+    const myText = "This is a simple snippet example";
+
+    const myObject = {
+        name: 'SvelteKit',
+        type: 'Framework',}
+
+    const myNestedObject = {
+      banana: {
+        name: 'banana',
+        color: 'yellow',
+      },
+      apple: {
+        name: 'apple',
+        color: 'red',
+      }
+    }
 </script>
+{#snippet simpleSnippet(t: string)}
+	<p>{t}</p>
+{/snippet}
+
+{#snippet advancedSnippet(o: Record<string, string>)}
+	{#each Object.entries(o) as [key, value]}
+    <p>{key}: {value}</p>
+  {/each}
+{/snippet}
+
+{#snippet superAdvancedSnippet(n: Record<string, Record<string, string>>)}
+  {#each Object.values(n) as nested}
+	{#each Object.entries(nested) as [key, value]}
+    <p>{key}: {value}</p>
+  {/each}
+  {/each}
+{/snippet}
 
 <p><a class="text-blue-600" href="/path?locale={page.data.locale}">Go to sub</a></p>
 <h1>Welcome to SvelteKit</h1>
 <p>{greet()}</p>
 <p>{page.data.layoutMsg}</p>
+
+{@render simpleSnippet(myText)}
+
+{@render advancedSnippet(myObject)}
+
+{@render superAdvancedSnippet(myNestedObject)}
+
 <p>Visit <a class="text-blue-600" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
