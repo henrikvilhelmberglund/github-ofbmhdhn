7791e6f1f829449c4bee2fb768acdead59ae27fc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2025-11-19 17:27:24 +0100

    chore:

--- src/locales/.wuchale/main.main.es.compiled.js
+++ src/locales/.wuchale/main.main.es.compiled.js
@@ -1,1 +1,15 @@
-export let c = ["Esta es una subruta","Ir a sub","Bienvenido a SvelteKit",["Visita ",[0,"svelte.dev/docs/kit"]," para leer la documentación"],"Hola desde el módulo","Cargar mensajes desde el estado de la página","Cargar mensajes desde props/data","SvelteKit","Framework","This is a simple snippet example","¡Hola desde el layout!","¡Hola desde el diseño anidado!"]
+
+            export let c = ["Esta es una subruta","Ir a sub","Bienvenido a SvelteKit",["Visita ",[0,"svelte.dev/docs/kit"]," para leer la documentación"],"Hola desde el módulo","Cargar mensajes desde el estado de la página","Cargar mensajes desde props/data","SvelteKit español","Framework español","This is a simple snippet example español","¡Hola desde el layout!","¡Hola desde el diseño anidado!"]
+            // only during dev, for HMR
+            let latestVersion = -1
+            // @ts-ignore
+            export function update({ version, data }) {
+                if (latestVersion >= version) {
+                    return
+                }
+                for (const [ index, item ] of data['es'] ?? []) {
+                    c[index] = item
+                }
+                latestVersion = version
+            }
+        
